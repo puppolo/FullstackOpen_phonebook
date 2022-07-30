0288d71417d4d6f90f33321089d6a4a8db4edab4
puppolo <nontanat.nutsati@gmail.com>
2022-07-30 13:19:06 +0700

    finished ex 3.9-3.11

--- src/services/service.js
+++ src/services/service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 //const baseURL = 'http://localhost:3001/api/persons'
-const baseURL = 'https://vast-bayou-31580.herokuapp.com'
+const baseURL = 'https://vast-bayou-31580.herokuapp.com/api/persons'
+//const baseURL = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseURL)
